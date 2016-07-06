3a8ea359ae03525a46dc5168c60453c90c383aee
CiaranWebDev <ciaranwebdev@gmail.com>
2016-07-06 18:42:30 +1200

    checkit

---
+++ lfcnz/js/script.js
@@ -0,0 +1,33 @@
+$(function(){
+
+
+
+
+
+	
+
+	$(".toggleNav").click(function() {
+  $("#subnav").toggleClass("active");
+  $(".toggleNavButton").toggleClass("active");
+});
+
+
+
+
+});
+
+$(function(){
+
+
+$(".fancybox").fancybox({	
+
+wrapCSS : "fancybox-custom",
+closeClick : true,
+openEffect: 'elastic',                   
+helpers : {title:{type:'inside'}, 
+
+
+}
+});
+
+});
